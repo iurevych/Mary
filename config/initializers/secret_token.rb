c685a3088a3e39dc978416cd4d6b0597fd2527a4
iurevych <iurevych@gmail.com>
2012-12-05 10:33:40 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mary::Application.config.secret_token = '05318773c0337a8783a1436ee42c12dca4cf4dae60f256eb64f92c2dafbf053226e3d3969d8c8f6894e3fd90c62420aab54f99da13a3cd78ec5c2c6ba6a8466b'
